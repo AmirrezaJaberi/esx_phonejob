4afca8b98cfa5439367b383aac8e30a23d847911
Amirreza Jaberi <83431690+AmIrReZa386@users.noreply.github.com>
2021-08-09 21:21:27 +0430

    Add ESX Legasy option + Fix Load Player Data

--- client/main.lua
+++ client/main.lua
@@ -1,21 +1,8 @@
-ESX                             = nil
 local PlayerData                = {}
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-
-	while ESX.GetPlayerData().job == nil do
-		Citizen.Wait(10)
-	end
-
-	PlayerData = ESX.GetPlayerData()
-end)
 
 RegisterNetEvent('esx:setJob')
 AddEventHandler('esx:setJob', function(job)
-	PlayerData.job = job
+    PlayerData.job = job
 end)
 
 Citizen.CreateThread(function ()
